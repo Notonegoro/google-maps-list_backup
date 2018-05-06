6f5f9f71725818bf5a245b559de2a77ac36e8f45
Notonegoro <38545264+Notonegoro@users.noreply.github.com>
2018-05-06 20:40:09 +0700

    Create main.py
    just creating the basics and playing with options

---
+++ main.py
@@ -0,0 +1,16 @@
+from pynput.mouse import Button, Controller
+from pillow import ImageColor as ImgColor
+
+mouse = Controller()
+
+# variables
+xColorContinue =
+yColorContinue = 
+xYellow1 =
+yYellow1 =
+xYellow2 =
+yYellow2 =
+AdressbarClip =
+PlaceDetails =
+
+
